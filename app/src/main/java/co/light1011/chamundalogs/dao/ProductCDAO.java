18e7e64cade0264110687397f262d338c89e90ed
amitrohan <amit@kruzr.co>
2021-11-06 17:04:25 +0530

    Cleanup

--- app/src/main/java/co/light1011/chamundalogs/dao/ProductCDAO.java
+++ app/src/main/java/co/light1011/chamundalogs/dao/ProductCDAO.java
@@ -8,7 +8,7 @@ import androidx.room.Query;
 
 import java.util.ArrayList;
 
-import co.light1011.chamundalogs.model.TableC;
+import co.light1011.chamundalogs.model.ProductC;
 
 @Dao
 public interface ProductCDAO {
@@ -16,11 +16,11 @@ public interface ProductCDAO {
     // allowing the insert of the same word multiple times by passing a
     // conflict resolution strategy
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    void insert(TableC table);
+    void insert(ProductC table);
 
     @Query("DELETE FROM productC_table")
     void deleteAll();
 
     @Query("SELECT * FROM productC_table ORDER BY id ASC")
-    MutableLiveData<ArrayList<TableC>> getProducts();
+    MutableLiveData<ArrayList<ProductC>> getProducts();
 }
--- app/src/main/java/co/light1011/chamundalogs/dao/UserCDAO.java
+++ app/src/main/java/co/light1011/chamundalogs/dao/UserCDAO.java
@@ -8,7 +8,7 @@ import androidx.room.Query;
 
 import java.util.ArrayList;
 
-import co.light1011.chamundalogs.model.TableC;
+import co.light1011.chamundalogs.model.UserC;
 
 @Dao
 public interface UserCDAO {
@@ -16,11 +16,11 @@ public interface UserCDAO {
     // allowing the insert of the same word multiple times by passing a
     // conflict resolution strategy
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    void insert(TableC table);
+    void insert(UserC table);
 
     @Query("DELETE FROM userC_table")
     void deleteAll();
 
     @Query("SELECT * FROM userC_table ORDER BY id ASC")
-    MutableLiveData<ArrayList<TableC>> getUsers();
+    MutableLiveData<ArrayList<UserC>> getUsers();
 }
