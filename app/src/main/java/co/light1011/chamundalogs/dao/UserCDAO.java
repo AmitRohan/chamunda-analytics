d02fdc9f2ad2ac01931dcdd9d43069aa93c94960
amitrohan <amit@kruzr.co>
2021-11-07 14:04:38 +0530

    Added few selectors

--- app/src/main/java/co/light1011/chamundalogs/dao/UserCDAO.java
+++ app/src/main/java/co/light1011/chamundalogs/dao/UserCDAO.java
@@ -24,4 +24,10 @@ public interface UserCDAO {
 
     @Query("SELECT * FROM userC_table ORDER BY id ASC")
     LiveData<List<UserC>> getUsers();
+
+    @Query("SELECT * FROM userC_table WHERE id=:userId")
+    LiveData<List<UserC>> getUsersById(String userId);
+
+    @Query("SELECT * FROM userC_table WHERE name=:userName")
+    LiveData<List<UserC>> getUsersByName(String userName);
 }
