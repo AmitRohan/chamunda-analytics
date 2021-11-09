2360b6b37ca61e8b58b9746ac2a3754dc158e992
amitrohan <amit@kruzr.co>
2021-11-09 19:56:53 +0530

    Replaced default graph with java flow

--- app/src/main/java/co/light1011/chamundalogs/MainActivity.java
+++ app/src/main/java/co/light1011/chamundalogs/MainActivity.java
@@ -1,30 +1,58 @@
 package co.light1011.chamundalogs;
 
 import android.os.Bundle;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+
+
+import co.light1011.chamundalogs.ui.dashboard.DashboardFragment;
+import co.light1011.chamundalogs.ui.products.ProductsFragment;
+import co.light1011.chamundalogs.ui.tables.TablesFragment;
+import co.light1011.chamundalogs.ui.users.UsersFragment;
+import co.light1011.chamundalogs.utils.ChamundaAnalyticsRepository;
 
 public class MainActivity extends AppCompatActivity {
+
+    DashboardFragment dashboardFragment;
+    ProductsFragment productsFragment;
+    TablesFragment  tablesFragment;
+    UsersFragment usersFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ChamundaAnalyticsRepository chamundaAnalyticsRepository =new ChamundaAnalyticsRepository(getApplication());
+
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_tables, R.id.navigation_dashboard, R.id.navigation_products, R.id.navigation_users)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+
+        dashboardFragment = new DashboardFragment();
+        productsFragment = new ProductsFragment(chamundaAnalyticsRepository);
+        tablesFragment = new TablesFragment(chamundaAnalyticsRepository);
+        usersFragment = new UsersFragment(chamundaAnalyticsRepository);
+
+
+        navView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.navigation_tables:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, tablesFragment).commit();
+                    return true;
+                case R.id.navigation_dashboard:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, dashboardFragment).commit();
+                    return true;
+                case R.id.navigation_products:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, productsFragment).commit();
+                    return true;
+                case R.id.navigation_users:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, usersFragment).commit();
+                    return true;
+            }
+            return false;
+        });
+        navView.setSelectedItemId(R.id.navigation_tables);
+
     }
 
 }
